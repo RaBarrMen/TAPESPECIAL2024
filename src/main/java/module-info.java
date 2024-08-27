cba04022aa970655f0c4d6029cac57c3ee4704dd
rafiux <rafaelbarramen@gmail.com>
2024-08-27 17:29:04 -0600

    creacion del repo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.tapespecial2024 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.tapespecial2024 to javafx.fxml;
+    exports org.example.tapespecial2024;
+}
