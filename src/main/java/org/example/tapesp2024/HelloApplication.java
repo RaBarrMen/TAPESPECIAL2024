1db13e9110f440b9012082b218ef09dd1b84e765
rafiux <rafaelbarramen@gmail.com>
2024-08-29 18:44:39 -0600

    Calc

--- src/main/java/org/example/tapesp2024/HelloApplication.java
+++ src/main/java/org/example/tapesp2024/HelloApplication.java
@@ -3,6 +3,10 @@ package org.example.tapesp2024;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -21,31 +25,34 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
-    private Button btn1, btn2, btn3;
-    private VBox vbox;
-    private HBox hbox;
+    private BorderPane border_principal;
+    private MenuBar menu_bar_principal;
+    private Menu menu_competencia1, menu_competencia2, salida;
+    private MenuItem menu_item_calculadora;
 
     public void CrearUI(){
-        btn1 = new Button("Button 1");
-        btn2 = new Button("Button 2");
-        btn3 = new Button("Button 3");
-        vbox = new VBox(btn1, btn2, btn3);
-        hbox = new HBox(btn1, btn2, btn3);
+        menu_item_calculadora = new MenuItem("Calculadora");
+        menu_item_calculadora.setOnAction(actionEvent -> new calculadora());
+
+        menu_competencia1 = new Menu("Competencia 1");
+        menu_competencia1.getItems().addAll(menu_item_calculadora);
+
+        menu_bar_principal = new MenuBar(menu_competencia1);
+        border_principal = new BorderPane();
+        border_principal.setTop(menu_bar_principal);
     }
     @Override
     public void start(Stage stage) throws IOException {
         // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         CrearUI();
-        Scene scene = new Scene(hbox, 320, 240);
+        Scene scene = new Scene(border_principal, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
-
-        new calculadora();
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         launch();
     }
 }
--- src/main/java/org/example/tapesp2024/vistas/calculadora.java
+++ src/main/java/org/example/tapesp2024/vistas/calculadora.java
@@ -1,5 +1,6 @@
 package org.example.tapesp2024.vistas;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -11,22 +12,35 @@ import javafx.stage.Stage;
 
 public class calculadora extends Stage {
 
-    private Button[] arrbtns = new Button[16];
+    private Button[][] arrbtns;
     private TextField txtpantalla;
     private GridPane gpdteclado;
     private VBox vBox;
     private Scene escena;
+    private String[] strteclas = {"7", "8", "9", "*", "4", "5", "6", "/", "1", "2", "3", "+", "0", ".", "=", "-" };
 
     private void CrearUI(){
-        arrbtns = new Button[16];
+        arrbtns = new Button[4][4];
         txtpantalla = new TextField("0");
+        txtpantalla.setAlignment(Pos.CENTER_RIGHT);
+        txtpantalla.setEditable(false);
         gpdteclado = new GridPane();
+        CrearTeclado();
         vBox = new VBox(txtpantalla, gpdteclado);
         escena = new Scene(vBox, 200, 200);
     }
 
     private void CrearTeclado(){
-        gpdteclado.set
+        for (int i = 0; i < arrbtns.length; i++) {
+            for (int j = 0; j < arrbtns.length; j++) {
+                arrbtns[j][i] = new Button(strteclas[4*i+j]);
+                arrbtns[j][i].setPrefSize(50,50);
+                int finalI = i;
+                int finalJ = j;
+                arrbtns[j][i].setOnAction(actionEvent -> detectar_tecla(strteclas[4* finalI + finalJ]));
+                gpdteclado.add(arrbtns[j][i],j,i);
+            }
+        }
     }
 
     public calculadora() {
@@ -34,5 +48,10 @@ public class calculadora extends Stage {
         this.setTitle("Calculadora jitomatera");
         this.setScene(escena);
         this.show();
+    }
+
+    private void detectar_tecla(String tecla){
+        //txtpantalla.clear();
+        txtpantalla.appendText(tecla);
     }
 }
